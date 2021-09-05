a93448d820f74401d258ed1968234592b094cd81
Mahmoud Noor <59361888+MahmoudNoor917@users.noreply.github.com>
2021-09-05 14:02:51 +0200

    Update YouTube Video Downloader.py

--- YouTube Video Downloader.py
+++ YouTube Video Downloader.py
@@ -1,8 +1,14 @@
 import pafy
 import os
 from moviepy.editor import *
+import requests
+from bs4 import BeautifulSoup
+import re
 
 
+#link = "https://www.youtube.com/watch?v=11CG88oR0Fg"
+
+#playlist = "https://www.youtube.com/playlist?list=PLvyTazcWChK8S7iF7XUG1XwnFfakh0PBf"
 download_path = os.path.dirname(os.path.realpath(__file__))
 
 def getfilewithextension(title,files=[]):
@@ -50,5 +56,54 @@ def download_single_video():
         print(files)
         combine_audio_video(files,video_name)
 
+def get_playlist_videoes(purl):
+    html_page = requests.get(purl)
+    soup = BeautifulSoup(html_page.content,'html.parser')
+    web = str(soup.prettify())
+    links = re.findall(r"/watch..=...........\\.........\=..................................",web)
+    links = list(set(links))
+    return links
 
-download_single_video()
+def download_playlist():
+    l = input("enter YouTube playlist URL you want to download : ")
+    links = get_playlist_videoes(l)
+    isaudio = input("do you want to download this playlist as audio : ")
+    quality = -1
+    for link in links:
+        video = pafy.new(link)
+        video_streams = video.videostreams
+        audio_streams = video.audiostreams
+        video_name = video.title
+
+        if(isaudio =='y' or isaudio=='Y'):
+            if(quality == -1):
+                for c,i in enumerate(audio_streams):
+                    print(f"{c+1}- {str(i).split('@')[-1]}   {str(i).split('@')[0].split(':')[-1]}")
+                quality = int(input("enter number of quality you want to download : "))
+        else:
+            if(quality == -1):
+                for c,i in enumerate(video_streams):
+                    print(f"{c+1}- {str(i).split('x')[-1]}   {str(i).split('@')[0].split(':')[-1]}")
+                quality = int(input("enter number of quality you want to download : "))
+        
+        if(isaudio =='y' or isaudio=='Y'):
+            if(audio_streams.length<quality):
+                video.getbestaudio().download(download_path)
+            else:
+                audio_streams[quality - 1].download(download_path)
+        else:
+            video.getbestaudio().download(download_path)
+            files = getfilewithextension(video_name)
+            if(video_streams.length<quality):
+                video.getbestvideo().download(download_path)
+            else:
+                video_streams[quality - 1].download(download_path)
+            files += getfilewithextension(video_name,files)
+            combine_audio_video(files,video_name)
+        
+
+choice = input("do you want to download a playlist")
+if(choice == 'y' or choice == 'Y'):
+    download_playlist()
+else:
+    download_single_video()
